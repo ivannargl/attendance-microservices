833940b89cd2d021b4f5fd75a92af8d4045bfff7
Ivanna Guz <r.guzmanlopez89@gmail.com>
2025-11-04 17:22:56 -0600

    first commit

---
+++ academic-ms/src/main/java/mx/edu/uteq/idgs12/academic_ms/AcademicMsApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs12.academic_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicMsApplication.class, args);
+	}
+
+}
---
+++ academic-ms/src/test/java/mx/edu/uteq/idgs12/academic_ms/AcademicMsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs12.academic_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

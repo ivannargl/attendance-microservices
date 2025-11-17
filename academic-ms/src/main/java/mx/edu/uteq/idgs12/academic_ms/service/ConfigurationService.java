5b006f296c9db5b75cce5ed07764077c23327c90
ivannargl <r.guzmanlopez89@gmail.com>
2025-11-17 01:10:37 -0600

    Crear una nueva configuracion

--- academic-ms/src/main/java/mx/edu/uteq/idgs12/academic_ms/service/ConfigurationService.java
+++ academic-ms/src/main/java/mx/edu/uteq/idgs12/academic_ms/service/ConfigurationService.java
@@ -1,19 +1,26 @@
 package mx.edu.uteq.idgs12.academic_ms.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import mx.edu.uteq.idgs12.academic_ms.dto.ConfigurationDTO;
 import mx.edu.uteq.idgs12.academic_ms.entity.Configuration;
+import mx.edu.uteq.idgs12.academic_ms.entity.University;
 import mx.edu.uteq.idgs12.academic_ms.repository.ConfigurationRepository;
+import mx.edu.uteq.idgs12.academic_ms.repository.UniversityRepository;
+
 
 @Service
 public class ConfigurationService {
 
     private final ConfigurationRepository configurationRepository;
+    private final UniversityRepository universityRepository;
 
-    public ConfigurationService(ConfigurationRepository configurationRepository) {
+    public ConfigurationService(ConfigurationRepository configurationRepository, UniversityRepository universityRepository) {
         this.configurationRepository = configurationRepository;
+        this.universityRepository = universityRepository;
     }
 
     public List<Configuration> getAll() {
@@ -26,5 +33,19 @@ public class ConfigurationService {
 
     public List<Configuration> getByUniversity(Integer idUniversity) {
         return configurationRepository.findByUniversity_IdUniversity(idUniversity);
+    }
+
+        public Configuration save(ConfigurationDTO dto) {
+        University university = universityRepository.findById(dto.getIdUniversity())
+                .orElseThrow(() -> new RuntimeException("University not found with ID: " + dto.getIdUniversity()));
+
+        Configuration config = new Configuration();
+        config.setIdConfiguration(dto.getIdConfiguration());
+        config.setUniversity(university);
+        config.setParameterName(dto.getParameterName());
+        config.setParameterValue(dto.getParameterValue());
+        config.setDescription(dto.getDescription());
+
+        return configurationRepository.save(config);
     }
 }
